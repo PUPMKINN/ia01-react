e827b43a5810441fc558e9f459e073710f6bfcbd
Trịnh Xuân Bách <trinhxuanbac2003cm@gmail.com>
2024-10-22 11:51:39 +0700

    add

---
+++ README.md
@@ -0,0 +1,63 @@
+# HCMUS Advanced Web App Programming
+
+**IA01 - React tutorial**
+
+21127011 - Trịnh Xuân Bách
+
+## How to view the project
+
+Clone the repository and run the following commands:
+
+```bash
+npm install
+npm run dev
+```
+
+or visit the [hosted version](http://localhost:5173/IA01-React-Tutorial/).
+
+## Self evaluation
+
+<table>
+    <thead>
+        <tr>
+            <th colspan=2>Feature</th>
+            <th>Pt.</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>For the current move only, show “You are at move #…” instead of a button</td>
+            <td>&check;</td>
+            <td>1.8</td>
+        </tr>
+        <tr>
+            <td>Rewrite the Board to use two loops to make the squares instead of hardcoding them</td>
+            <td>&check;</td>
+            <td>1.8</td>
+        </tr>
+        <tr>
+            <td>Add a toggle button that lets you sort the moves in either ascending or descending order</td>
+            <td>&check;</td>
+            <td>1.8</td>
+        </tr>
+        <tr>
+            <td>When someone wins, highlight the three squares that caused the win</td>
+            <td>&check;</td>
+            <td>1.8</td>
+        </tr>
+        <tr>
+            <td>Display the location for each move in the format (row, col) in the move history list</td>
+            <td>&check;</td>
+            <td>1.8</td>
+        </tr>
+        <tr>
+            <td>Upload to a public host</td>
+            <td>&check;</td>
+            <td aligncenter>1</td>
+        </tr>
+        <tr>
+            <td colspan=2 aligncenter><strong>Total</strong></td>
+            <td><strong>10</strong></td>
+        </tr>
+    </tbody>
+</table>
---
+++ src/Game.jsx
@@ -0,0 +1,184 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick, isWinningSquare }) {
+    return (
+        <button className={`square ${isWinningSquare ? "winning-square" : ""}`} onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+        if (calculateWinner(squares).winner || squares[i]) {
+            return;
+        }
+
+        const nextSquares = squares.slice();
+
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+
+        const location = { row: Math.floor(i / 3) + 1, col: (i % 3) + 1 };
+
+        onPlay(nextSquares, location);
+    }
+
+    let status;
+
+    const winnerInfo = calculateWinner(squares);
+    const winner = winnerInfo.winner;
+    const winningSquares = winnerInfo.winningSquares || [];
+
+    const isDraw = winnerInfo.draw;
+
+    if (winner) {
+        status = "Winner: " + winner;
+    } else if (isDraw) {
+        status = "The game is a draw.";
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    function renderSquare(i) {
+        return (
+            <Square
+                key={i}
+                value={squares[i]}
+                onSquareClick={() => handleClick(i)}
+                isWinningSquare={winningSquares.includes(i)}
+            />
+        );
+    }
+
+    const boardRows = [];
+
+    for (let row = 0; row < 3; row++) {
+        const boardSquares = [];
+
+        for (let col = 0; col < 3; col++) {
+            boardSquares.push(renderSquare(row * 3 + col));
+        }
+
+        boardRows.push(
+            <div key={row} className="board-row">
+                {boardSquares}
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <div className="status">{status}</div>
+            {boardRows}
+        </>
+    );
+}
+
+export default function Game() {
+    const [history, setHistory] = useState([{ squares: Array(9).fill(null), location: null }]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const [isAscending, setIsAscending] = useState(true);
+
+    const xIsNext = currentMove % 2 === 0;
+
+    const currentSquares = history[currentMove].squares;
+
+    function handlePlay(nextSquares, location) {
+        const nextHistory = [...history.slice(0, currentMove + 1), { squares: nextSquares, location }];
+
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const toggleSortOrder = () => {
+        setIsAscending(!isAscending);
+    };
+
+    const moves = history.map((step, move) => {
+        const desc = move ? `Go to move #${move} (${step.location.row}, ${step.location.col})` : "Go to game start";
+
+        return (
+            <li key={move}>
+                {move === currentMove ? (
+                    <span>
+                        You are at move #{move} {step.location ? `(${step.location.row}, ${step.location.col})` : ""}
+                    </span>
+                ) : (
+                    <button onClick={() => jumpTo(move)}>{desc}</button>
+                )}
+            </li>
+        );
+    });
+
+    if (!isAscending) {
+        moves.reverse();
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+
+            <div className="game-info">
+                <button onClick={toggleSortOrder}>{isAscending ? "Sort Descending ↓" : "Sort Ascending ↑"}</button>
+
+                <ul>{moves}</ul>
+            </div>
+
+            <footer>
+                <p>
+                    <strong>HCMUS Advanced Web Programming</strong>
+                </p>
+
+                <p>
+                    <a href="https://github.com/PUPMKINN/IA01-React-Tutorial" target="_blank">
+                        <strong>IA01-React Tutorial &#8599;</strong>
+                    </a>
+                </p>
+
+                <p>
+                    By{" "}
+                    <a href="https://github.com/PUPMKINN" target="_blank">
+                        Trịnh Xuân Bách
+                    </a>
+                </p>
+            </footer>
+        </div>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return { winner: squares[a], winningSquares: [a, b, c] };
+        }
+    }
+
+    if (squares.every((square) => square !== null)) {
+        return { winner: null, draw: true, winningSquares: [] };
+    }
+
+    return { winner: null, winningSquares: [] };
+}
